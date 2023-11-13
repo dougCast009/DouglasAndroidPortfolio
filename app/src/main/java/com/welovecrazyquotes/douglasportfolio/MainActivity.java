1bc5f8d28330ad474bcc9293d1192404996bc06f
dougCast009 <docastillo@bp.fi.cr>
2023-11-13 16:55:41 -0600

    Estudio 13/11/2023

--- app/src/main/java/com/welovecrazyquotes/douglasportfolio/MainActivity.java
+++ app/src/main/java/com/welovecrazyquotes/douglasportfolio/MainActivity.java
@@ -10,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Project[] projects = {
+                new Project("Getting Started App", "Our very first project, the defaul 'Hello World app'!", R.drawable.getting_started),
+                new Project("Vin Quote", "Our very first project, the defaul 'Hello World app'!", R.drawable.quote),
+                new Project("BMI Calculator", "Our very first project, the defaul 'Hello World app'!", R.drawable.calculator),
+                new Project("Inches Converter", "Our very first project, the defaul 'Hello World app'!", R.drawable.tape),
+                new Project("The Hungry Developer", "Our very first project, the defaul 'Hello World app'!", R.drawable.hungry_developer)
+        };
+
     }
 }
